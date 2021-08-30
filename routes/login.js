e7baae3d9a944c758a2a1410789a73b274c4adbc
Tim McCarthy <thetimmccarthy@gmail.com>
2021-08-30 17:12:03 -0500

    wip

--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,6 @@ var userModel = require('../models/user')
 /* GET users listing. */
 router.get('/', loginController.login_get);
 
-router.post('/', loginController.login_id_get)
+router.post('/', loginController.login_post)
 
 module.exports = router;
